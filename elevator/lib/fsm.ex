7f92a8a5a99c1601872ddba2d0e5913775386d6a
Dodern <jacob.hemstad.gimle@gmail.com>
2020-03-05 11:50:56 +0100

    FSM almost finished, missing dets

--- elevator/lib/fsm.ex
+++ elevator/lib/fsm.ex
@@ -8,59 +8,63 @@ defmodule ElevatorFSM do
     {:ok, _pid} =
       GenStateMachine.start_link(
         ElevatorFSM,
-        {:idle, {Driver.get_floor_sensor_state(), [3, 2, 1]}},
+        {:at_floor,
+         {Driver.get_floor_sensor_state(), [{3, :hall_up}, {1, :hall_down}, {2, :cab}]}},
         name: __MODULE__
       )
   end
 
-  def idle(:enter, _, data) do
+  def at_floor(:enter, _, data) do
     floor = elem(data, 0)
     orders = elem(data, 1)
     Driver.set_floor_indicator(floor)
-    IO.inspect("Entering idle mode")
+    IO.inspect("Entering at_floor mode")
     IO.inspect("Checking for orders")
     IO.inspect(orders, label: "The current orders are")
     IO.inspect("Current floor is #{floor}")
 
     if length(orders) > 0 do
       IO.inspect("Executing order")
+      [head | _tail] = orders
+      current_order = elem(head, 0)
+      current_order_direction = elem(head, 1)
 
       cond do
-        floor == Enum.at(orders, 0) ->
+        floor == current_order ->
+          Driver.set_order_button_light(current_order_direction, floor, :off)
           Driver.set_motor_direction(:stop)
           open_door()
 
-        floor < Enum.at(orders, 0) ->
+        floor < current_order ->
           move_up()
 
-        floor > Enum.at(orders, 0) ->
+        floor > current_order ->
           move_down()
       end
     else
       IO.inspect("Waiting for new orders")
-      update_orders([3, 1])
     end
 
     {:keep_state_and_data, [{:state_timeout, 1000, :waiting_for_orders}]}
   end
 
-  def idle(:state_timeout, :waiting_for_orders, _data) do
+  def at_floor(:state_timeout, :waiting_for_orders, _data) do
     :repeat_state_and_data
   end
 
-  def idle(:cast, :up, data) do
+  def at_floor(:cast, :up, data) do
     IO.inspect("Moving upwards")
     Driver.set_motor_direction(:up)
     {:next_state, :moving_past_floor, data}
   end
 
-  def idle(:cast, :down, data) do
+  def at_floor(:cast, :down, data) do
     IO.inspect("Moving downwards")
     Driver.set_motor_direction(:down)
     {:next_state, :moving_past_floor, data}
   end
 
-  def idle(:cast, :open_door, data) do
+  def at_floor(:cast, :open_door, data) do
     floor = elem(data, 0)
     orders = elem(data, 1)
     [_head | tail] = orders
@@ -71,7 +75,7 @@ defmodule ElevatorFSM do
     {:next_state, :door_open, data}
   end
 
-  def idle(:cast, {:update_orders, new_orders}, data) do
+  def at_floor(:cast, {:update_orders, new_orders}, data) do
     floor = elem(data, 0)
     orders = new_orders
     data = {floor, orders}
@@ -84,12 +88,23 @@ defmodule ElevatorFSM do
     Process.sleep(3000)
     IO.inspect("Closing doors")
     Driver.set_door_open_light(:off)
-    idle()
+    at_floor()
     :keep_state_and_data
   end
 
-  def door_open(:cast, :idle, data) do
-    {:next_state, :idle, data}
+  def door_open(:state_timeout, :door_open, _data) do
+    :repeat_state_and_data
+  end
+
+  def door_open(:cast, :at_floor, data) do
+    {:next_state, :at_floor, data}
+  end
+
+  def door_open(:cast, {:update_orders, new_orders}, data) do
+    floor = elem(data, 0)
+    orders = new_orders
+    data = {floor, orders}
+    {:keep_state, data, [{:state_timeout, 100, :door_open}]}
   end
 
   def moving_past_floor(:enter, _, _data) do
@@ -141,7 +156,7 @@ defmodule ElevatorFSM do
     floor = Driver.get_floor_sensor_state()
     orders = elem(data, 1)
     data = {floor, orders}
-    {:next_state, :idle, data}
+    {:next_state, :at_floor, data}
   end
 
   def moving_between_floors(:cast, {:update_orders, new_orders}, data) do
@@ -153,10 +168,6 @@ defmodule ElevatorFSM do
 
   def update_orders(new_orders) do
     GenStateMachine.cast(__MODULE__, {:update_orders, new_orders})
-  end
-
-  def idle do
-    GenStateMachine.cast(__MODULE__, :idle)
   end
 
   def move_up do
